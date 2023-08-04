72de7263e3384f3e2bca77b915c58288a8486293
Dimas NP <dimsdeall@gmail.com>
2023-08-04 14:06:55 +0700

    fix:vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,8 +23,8 @@ export default function Home() {
       url: "https://redux-toolkit.js.org/rtk-query/overview",
     },
     {
-      name: "Firebase Hosting",
-      url: "https://firebase.google.com/docs/hosting",
+      name: "Vercel",
+      url: "https://vercel.com",
     },
   ];
 
